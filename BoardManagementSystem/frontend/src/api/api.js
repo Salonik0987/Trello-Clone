b775c166c5ed4e5c366c47dbf459dfa31d594e07
Saloni Kumari <cec231344.cse.cec@cgc.edu.in>
2026-03-29 16:33:34 +0530

    Update api.js

--- BoardManagementSystem/frontend/src/api/api.js
+++ BoardManagementSystem/frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: "https://trello-clone-op1c.onrender.com" || "http://localhost:5000",
 });
 
 export default API;
