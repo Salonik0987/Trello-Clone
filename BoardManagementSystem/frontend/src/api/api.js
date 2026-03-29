b73e7f28c97dfd58dfd61ab0529378e1bff0ae8d
Saloni Kumari <cec231344.cse.cec@cgc.edu.in>
2026-03-29 14:59:10 +0530

    Update api.js

--- BoardManagementSystem/frontend/src/api/api.js
+++ BoardManagementSystem/frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://trello-clone-backend-ihfe.onrender.com",
 });
 
 // Add request interceptor for debugging
